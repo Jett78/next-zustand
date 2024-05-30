9566e0c6313d109f67719245873ba5ba48457ca8
Jeet <jetdeula@gmail.com>
2024-05-30 18:58:17 +0545

    updated

---
+++ app/zustand/CourseStore.jsx
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import {devtools,persist} from "zustand/middleware"
+const CourseStore = (set) => ({
+    courses:[],  //initial state
+    addCourse:(course) => {
+        set((state) => ({
+            courses:[course,...state.courses],
+        }))
+    },
+    removeCourse:(courseId) => {
+        set((state) => ({
+            courses:state.courses.filter((c) => c.id!==courseId)
+        }))
+    },
+    toggleCourseStatus:(courseId) => {
+        set((state) => ({
+            courses:state.courses.map((course) => course.id === completed?{...course,completed:!course.completed}:course)
+        }))
+    }
+})
+
+const useCourseStore = create(
+    devtools(
+        persist(CourseStore,{
+            name:"courses"
+        })
+    )
+)
+
+
+export default useCourseStore
+
+// const useCourseStore = create((set) => ({
+//     bears: 0,
+//     increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+//     decreasePopulation: () => set((state) => ({ bears: state.bears - 1 })),
+//     resetPopulation:() => set((state) => ({bears:state.bears=0}))
+// }));
+
+// export default useCourseStore
